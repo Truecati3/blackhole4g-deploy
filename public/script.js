c3d0bdb779d4dea76b29a2b58c17a98365b0e853
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2023-05-16 18:58:54 -0500

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,134 @@
+
+function yt() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-wuw%2Cymuvu%60e%2Ccmm-');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function youtube() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-dksaopd%2Ccmm-');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);
+        }
+
+    function ultraviolet() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-untpatimlgt%2Csggqo%2Cngt-');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function Interstellar() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-t83q.onrender.com');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function Interstellar2() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-r1n7.onrender.com');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function appnav() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://chinesefood.algebra.ultsrv.com/');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function home() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://black-hole4-fun.vercel.app');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function rammerhead1() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://rh.renaissance-go.ml/');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function rammerhead2() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://whentheca.shut.up.qwerty.com.ar/');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function rammerhead3() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://h.okzk.com');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function rammerhead4() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://h.okzk.com');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function rammerhead5() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://leshanisbald.propertyshots.com/');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+
+function rep() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-wuw%2Cx%60oz.aoo%2Fgn%2FUQ');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function voi() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-wuw%2Cx%60oz.aoo%2Fgn%2FUQ%2Frlcy%3Dxp%3Dqhglnncv');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function spot() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-orel.qpmtkf%7B.aoo%2F');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function mine() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://minecraftforneenah-games.vercel.app/');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function music() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://chess-game-nu.vercel.app');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function mp4() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://videohub-gilt.vercel.app');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function shred() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-sjrgdqawcg.aoo%2F');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function tubi() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-twbktt.aoo%2F');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+function coolmath() {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', 'https://interstellar-v4pw.onrender.com/service/hvtrs8%2F-wuw%2Cc');
+            iframe.setAttribute('style', 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;border: 0;z-index: 99999;');
+            document.body.appendChild(iframe);}
+
+
+
+function contact() {
+window.open('https://docs.google.com/forms/d/e/1FAIpQLScTehgxUnrj7FzLMXrB0D3Qz34uqCmk7o5sbDfnkfAH5MMPZw/viewform?usp=sf_link');
+}
